c6f064d74c0f7cf143f0ebadced2799730426eb8
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2021-04-10 15:40:33 +0100

    Added code to print policies and q values.

--- analysis/plots.py
+++ analysis/plots.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import random
 import numpy as np
 import pathlib
 
@@ -8,8 +9,8 @@ import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt
 import seaborn as sns
-
 plt.style.use('ggplot')
+
 FIGURE_X = 6.0
 FIGURE_Y = 4.0
 
@@ -17,7 +18,6 @@ DATA_FOLDER_PATH = str(pathlib.Path(__file__).parent.parent.absolute()) + '/data
 PLOTS_FOLDER_PATH = str(pathlib.Path(__file__).parent.absolute()) + '/plots/'
 
 EXP_IDS = ['8_8_q_learning_2021-04-09-18-04-25']
-
 VAL_ITER_DATA = '8_8_val_iter_2021-04-09-18-08-36'
 
 
@@ -62,6 +62,16 @@ def print_policy(policy, sizes):
         sys.stdout.write('|\n')
     sys.stdout.write('-' * (size_x + 2)+'\n')
 
+def plot_Q_vals(q_vals, sizes):
+    size_x, size_y = sizes[0], sizes[1]
+    sys.stdout.write('-'*(size_x+2)+'\n')
+    for h in range(size_y):
+        sys.stdout.write('|')
+        for w in range(size_x):
+            sys.stdout.write("{:.1f} ".format(q_vals[str(xy_to_idx((w,h),size_x, size_y))]))
+        sys.stdout.write('|\n')
+    sys.stdout.write('-' * (size_x + 2)+'\n')
+
 
 if __name__ == "__main__":
 
@@ -69,40 +79,50 @@ if __name__ == "__main__":
     os.makedirs(PLOTS_FOLDER_PATH, exist_ok=True)
 
     # Get args file.
+    # (Assumes all experiments share the same arguments).
     args = get_args_json_file(DATA_FOLDER_PATH + EXP_IDS[0])
 
-    # Load data.
+    # Load and parse data.
     data = {}
     for exp_name in EXP_IDS:
 
+        # Open data.
         exp_path = DATA_FOLDER_PATH + exp_name
         with open(exp_path + "/train_data.json", 'r') as f:
             exp_data = json.load(f)
             exp_data = json.loads(exp_data)
         f.close()
 
-        # Average data for each train run.
-        averaged_data = {}
+        # Parse data for each train run.
+        parsed_data = {}
 
-        # episode_rewards
+        # episode_rewards field.
         r_concatenated = np.array([exp['episode_rewards'] for exp in exp_data])
         r_concatenated = np.mean(r_concatenated, axis=0)
-        averaged_data['episode_rewards'] = r_concatenated
+        parsed_data['episode_rewards'] = r_concatenated
 
-        # epsilon_values
+        # epsilon_values field.
         e_concatenated = np.array([exp['epsilon_values'] for exp in exp_data])
         e_concatenated = np.mean(e_concatenated, axis=0)
-        averaged_data['epsilon_values'] = e_concatenated
+        parsed_data['epsilon_values'] = e_concatenated
 
-        # Q_vals
+        # Q_vals field.
         q_concatenated = np.array([exp['Q_vals'] for exp in exp_data])
         q_concatenated = np.mean(q_concatenated, axis=0)
-        averaged_data['Q_vals'] = q_concatenated
+        parsed_data['Q_vals'] = q_concatenated
 
-        data[exp_name] = averaged_data
+        # max_Q_vals field.
+        max_Q_vals = np.array([exp['max_Q_vals'] for exp in exp_data])
+        parsed_data['max_Q_vals'] = max_Q_vals
+
+        # policy field.
+        policies = np.array([exp['policy'] for exp in exp_data])
+        parsed_data['policies'] = policies
+
+        data[exp_name] = parsed_data
 
     if VAL_ITER_DATA:
-        # Load optimal Q-values.
+        # Load optimal policy/Q-values.
         val_iter_path = DATA_FOLDER_PATH + VAL_ITER_DATA
         with open(val_iter_path + "/train_data.json", 'r') as f:
             val_iter_data = json.load(f)
@@ -149,24 +169,29 @@ if __name__ == "__main__":
     plt.close()
 
     """
-        Plot policy.
+        Plot policies.
     """
     if VAL_ITER_DATA:
         print(f'{VAL_ITER_DATA} policy:')
-        sizes = (args['env_args']['size_x'], args['env_args']['size_y'])
-        print_policy(val_iter_data['policy'], sizes)
+        print_policy(val_iter_data['policy'], (args['env_args']['size_x'], args['env_args']['size_y']))
 
-    # TODO: print the other policies.
+    for exp_id in EXP_IDS:
+        for i, policy in enumerate(data[exp_id]['policies']):
+            print(f'{VAL_ITER_DATA} policy (run {i}):')
+            print_policy(policy, (args['env_args']['size_x'], args['env_args']['size_y']))
 
-    # TODO: print the max Q vals.
+    """
+        Plot max Q-values.
+    """
+    print('\n')
+    if VAL_ITER_DATA:
+        print(f'{VAL_ITER_DATA} max Q-values:')
+        plot_Q_vals(val_iter_data['max_Q_vals'], (args['env_args']['size_x'], args['env_args']['size_y']))
 
-    """ # Plot max Q-values.
-    print('Max Q-vals:')
-    for h in range(size_y):
-        for w in range(size_x):
-            sys.stdout.write("{:.1f} ".format(max_Q_vals[xy_to_idx((w,h),size_x, size_y)]))
-        sys.stdout.write('\n')
-    sys.stdout.write('\n') """
+    for exp_id in EXP_IDS:
+        for i, qs in enumerate(data[exp_id]['max_Q_vals']):
+            print(f'{VAL_ITER_DATA} max Q-values (run {i}):')
+            plot_Q_vals(qs, (args['env_args']['size_x'], args['env_args']['size_y']))
 
     """
         Q-values plots.
