fda8ccae4973a75f4d693cf663659c198e48453b
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2021-04-29 17:15:11 +0100

    Changed exp ids.

--- plots_helper.py
+++ plots_helper.py
@@ -7,31 +7,28 @@ from analysis.plot_state_qval import main as Qvalplots
 # `VAL_ITER_DATA` must be set if `COMPUTE_PLOTS` is True.
 VAL_ITER_DATA = 'gridEnv4_val_iter_2021-04-28-09-54-18'
 
-EXP_IDS = ['gridEnv4_oracle_fqi_2021-04-28-14-50-24',
-'gridEnv4_oracle_fqi_2021-04-28-15-10-32',
-'gridEnv4_oracle_fqi_2021-04-28-15-29-45',
-'gridEnv4_oracle_fqi_2021-04-28-15-49-23',
-'gridEnv4_oracle_fqi_2021-04-28-16-09-00',
-'gridEnv4_oracle_fqi_2021-04-28-16-28-37',
-'gridEnv4_oracle_fqi_2021-04-28-16-47-55',
-'gridEnv4_oracle_fqi_2021-04-28-17-07-24',
-'gridEnv4_oracle_fqi_2021-04-28-17-27-00',
-'gridEnv4_oracle_fqi_2021-04-28-17-47-07',
-'gridEnv4_oracle_fqi_2021-04-28-18-07-07',
-'gridEnv4_oracle_fqi_2021-04-28-18-27-25',
-'gridEnv4_oracle_fqi_2021-04-28-18-47-34',
-'gridEnv4_oracle_fqi_2021-04-28-19-07-41',
-'gridEnv4_oracle_fqi_2021-04-28-19-27-40',
-'gridEnv4_oracle_fqi_2021-04-28-19-47-47',
-'gridEnv4_oracle_fqi_2021-04-28-20-07-51',
-'gridEnv4_oracle_fqi_2021-04-28-20-27-56',
-'gridEnv4_oracle_fqi_2021-04-28-20-48-05',
-'gridEnv4_oracle_fqi_2021-04-28-21-08-22',
-'gridEnv4_oracle_fqi_2021-04-28-21-28-33']
+EXP_IDS = [
+# One-hot observations.    
+'gridEnv4_oracle_fqi_2021-04-28-23-55-32',
+'gridEnv4_oracle_fqi_2021-04-29-00-17-56',
+'gridEnv4_oracle_fqi_2021-04-29-00-40-23',
+'gridEnv4_oracle_fqi_2021-04-29-01-03-29',
+'gridEnv4_oracle_fqi_2021-04-29-01-26-22',
+'gridEnv4_oracle_fqi_2021-04-29-01-49-16',
+'gridEnv4_oracle_fqi_2021-04-29-02-12-01',
+
+# Smoothed observations.    
+'gridEnv4_oracle_fqi_2021-04-29-02-34-52',
+'gridEnv4_oracle_fqi_2021-04-29-02-59-34',
+'gridEnv4_oracle_fqi_2021-04-29-03-25-15',
+'gridEnv4_oracle_fqi_2021-04-29-03-50-03',
+'gridEnv4_oracle_fqi_2021-04-29-04-14-35',
+'gridEnv4_oracle_fqi_2021-04-29-04-39-05',
+'gridEnv4_oracle_fqi_2021-04-29-05-03-42']
 
 if __name__ == "__main__":
 
     for exp_id in EXP_IDS:
         print(exp_id)
-        #plots(exp_id, VAL_ITER_DATA)
-        Qvalplots(exp_id, VAL_ITER_DATA)
+        plots(exp_id, VAL_ITER_DATA)
+        # Qvalplots(exp_id, VAL_ITER_DATA)
