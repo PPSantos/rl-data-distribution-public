0216c648a8f243b21d53ca2e52130503a0493978
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2021-05-25 17:06:34 +0100

    Deleted bounds.py file

--- gen_errors/gen_errors_entropy_plot.py
+++ gen_errors/gen_errors_entropy_plot.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
 
     # Entropy plot.
     alphas = [0.05, 0.1, 0.25, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 10.0] #, 20.0, 30.0]
-    num_classes = 100
+    num_classes = 5
     entropies = []
     for alpha in alphas:
         expected_entropy = scipy.special.digamma(num_classes*alpha + 1) - scipy.special.digamma(alpha + 1)
@@ -21,6 +21,6 @@ if __name__ == "__main__":
     fig = plt.figure()
     fig.set_size_inches(FIGURE_X, FIGURE_Y)
     plt.plot(alphas, entropies)
-    plt.xlabel('Alpha (Dirichelet parameter)')
+    plt.xlabel('Alpha (Dirichlet parameter)')
     plt.ylabel('Expected entropy of categorical dist.')
     plt.show()
