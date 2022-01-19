5f45a0131cc6779321a8eaa8522596801f5e7930
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2022-01-19 14:58:44 +0000

    run_multiple script tweaks

--- scripts/run_multiple.py
+++ scripts/run_multiple.py
@@ -8,8 +8,8 @@ ENVS = ['gridEnv1',]
 
 if __name__ == "__main__":
 
-    # Load default args.
-    run_args = RUN_ARGS
+    # Vary alpha dirichlet parameter.
+    """ run_args = RUN_ARGS
 
     exp_ids = []
     for env in ENVS:
@@ -17,7 +17,6 @@ if __name__ == "__main__":
         # Setup train args.
         run_args['env_name'] = env
         print('env=', env)
-
 
         run_args['dataset_args']['force_full_coverage'] = False
         for a in [0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0]:
@@ -37,6 +36,40 @@ if __name__ == "__main__":
 
             print('Alpha=', a)
             run_args['dataset_args']['dirichlet_dataset_args']['dirichlet_alpha_coef'] = a
+
+            # Run.
+            exp_id = run(run_args)
+            exp_ids.append(exp_id)
+
+            print('Exp. ids:', exp_ids) """
+
+    # Vary epsilon parameter.
+    run_args = RUN_ARGS
+
+    exp_ids = []
+    for env in ENVS:
+
+        # Setup train args.
+        run_args['env_name'] = env
+        print('env=', env)
+
+        run_args['dataset_args']['force_full_coverage'] = False
+        for e in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+
+            print('Epsilon=', e)
+            run_args['dataset_args']['eps_greedy_dataset_args']['epsilon'] = e
+
+            # Run.
+            exp_id = run(run_args)
+            exp_ids.append(exp_id)
+
+            print('Exp. ids:', exp_ids)
+
+        run_args['dataset_args']['force_full_coverage'] = True
+        for e in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
+
+            print('Epsilon=', e)
+            run_args['dataset_args']['eps_greedy_dataset_args']['epsilon'] = e
 
             # Run.
             exp_id = run(run_args)
