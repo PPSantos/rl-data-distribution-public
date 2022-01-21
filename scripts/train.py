50fabd79ab5792e9151e5093069662622bdaa44b
PPSantos <pedro.pinto.santos@tecnico.ulisboa.pt>
2022-01-21 10:01:05 +0000

    Fixed arg typo

--- scripts/train.py
+++ scripts/train.py
@@ -55,8 +55,8 @@ def train_run(run_args):
     time.sleep(time_delay)
 
     # Set number of threads.
-    if run_args['num_threads_per_proc']:
-        torch.set_num_threads(run_args['num_threads_per_proc'])
+    if args['num_threads_per_proc']:
+        torch.set_num_threads(args['num_threads_per_proc'])
 
     # Load environment.
     env, env_grid_spec = env_suite.get_env(args['env_name'])
